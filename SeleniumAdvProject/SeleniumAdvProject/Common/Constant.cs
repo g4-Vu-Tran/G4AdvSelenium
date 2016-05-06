717aa6d63478ecff7e66bb5c7c30621d00c27ff7
SelAdv1Mar-G4-huong-huynh <huonghuynh90@gmail.com>
2016-05-07 01:16:10 +0700

    update constant.cs

--- SeleniumAdvProject/SeleniumAdvProject/Common/Constant.cs
+++ SeleniumAdvProject/SeleniumAdvProject/Common/Constant.cs
@@ -6,7 +6,6 @@ namespace SeleniumAdvProject.Common
     {
         public static IWebDriver WebDriver;
         public const string LoginPageUrl = "http://192.168.0.102/TADashboard/login.jsp";
-        //public const string MailinatorPageUrl = "http://mailinator.com/inbox.jsp?to={0}";
         public const string Repository = "SampleRepository";
         public const string UserName = "administrator";
         public const string Password = "";
