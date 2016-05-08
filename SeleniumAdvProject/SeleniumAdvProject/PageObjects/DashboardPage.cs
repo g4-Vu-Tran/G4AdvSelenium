a90b06d24a9e912fef1ef979ab5bd18199b6664c
g4-Vu-Tran <vu.tran@logigear.com>
2016-05-08 19:14:34 +0700

    add TC45

--- SeleniumAdvProject/SeleniumAdvProject/PageObjects/DashboardPage.cs
+++ SeleniumAdvProject/SeleniumAdvProject/PageObjects/DashboardPage.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
 using SeleniumAdvProject.Common;
 using System;
 using System.Collections.Generic;
@@ -8,11 +9,14 @@ using System.Threading.Tasks;
 
 namespace SeleniumAdvProject.PageObjects
 {
-    public class DashboardPage:BasePage
+    public class DashboardPage : BasePage
     {
         #region Locators
 
-        static readonly By _lblUsername = By.XPath("//a[@href='#Welcome']");        
+        static readonly By _lblUsername = By.XPath("//a[@href='#Welcome']");
+        static readonly By _lblRepository = By.XPath("//a[@href='#Repository']");
+        static readonly By _lblCurrentRepository = By.XPath("//a[@href='#Repository']/span");
+        static readonly By _lnkLogout = By.XPath("//a[@href='logout.do']");
 
         #endregion
         #region Elements
@@ -20,6 +24,18 @@ namespace SeleniumAdvProject.PageObjects
         public IWebElement LblUsername
         {
             get { return Constant.WebDriver.FindElement(_lblUsername); }
+        }
+        public IWebElement LblRepository
+        {
+            get { return Constant.WebDriver.FindElement(_lblRepository); }
+        }
+        public IWebElement LblCurrentRepository
+        {
+            get { return Constant.WebDriver.FindElement(_lblCurrentRepository); }
+        }
+        public IWebElement LnkLogout
+        {
+            get { return Constant.WebDriver.FindElement(_lnkLogout); }
         }
 
         #endregion
@@ -30,11 +46,26 @@ namespace SeleniumAdvProject.PageObjects
             return LblUsername.Text;
         }
 
-        public void Logout()
+        public string GetCurrentRepositoryText()
         {
-
+            return LblCurrentRepository.Text;
         }
 
+        public LoginPage Logout()
+        {
+            Actions mouseAction = new Actions(Constant.WebDriver);
+            mouseAction.MoveToElement(LblUsername).Perform();
+            mouseAction.MoveToElement(LnkLogout).Click().Perform();
+            return new LoginPage();
+        }
+
+        public DashboardPage SelectRepository(String repositoryName)
+        {
+            Actions mouseAction = new Actions(Constant.WebDriver);
+            mouseAction.MoveToElement(LblRepository).Perform();
+            mouseAction.MoveToElement(Constant.WebDriver.FindElement(By.XPath("//ul[@id='ulListRepositories']//a[.='" + repositoryName + "']"))).Click().Perform();
+            return this;
+        }
 
         #endregion
     }
