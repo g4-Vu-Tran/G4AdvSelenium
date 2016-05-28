08d248caa34465131f15ef1462f6546a8bdea1d2
g4-Vu-Tran <vu.tran@logigear.com>
2016-05-28 20:56:05 +0700

    vu

--- SeleniumAdvProject/SeleniumAdvProject/Common/CommonAction.cs
+++ SeleniumAdvProject/SeleniumAdvProject/Common/CommonAction.cs
@@ -5,38 +5,26 @@ namespace SeleniumAdvProject.Common
 {
     public static class CommonAction
     {
-
         /// <summary>
-        /// Genrates the random string
+        /// Genrates the random string.
         /// </summary>
-        /// <param name="length">The length of string</param>
-        /// <returns>String</returns>
-        /// <author>Huong Huynh</author>
-        /// <date>05/26/2015</date>
-        public static string GenrateRandomString(int length = Constants.lenghtRandomString)
+        /// <param name="length">The length.</param>
+        /// <returns></returns>
+        public static string GenrateRandomString(int length)
         {
             return Guid.NewGuid().ToString().Substring(0, length);
         }
 
         /// <summary>
-        /// Randoms the string 
+        /// Randoms the day.
         /// </summary>
         /// <param name="day">The day.</param>
-        /// <returns>String</returns>
-        /// <author>Huong Huynh</author>
-        /// <date>05/26/2015</date>
+        /// <returns></returns>
         public static string RandomDay(int day)
         {
             return DateTime.Now.AddDays(day).ToString("M/dd/yyy");
         }
 
-        /// <summary>
-        /// Encodes the space to \u00a0
-        /// </summary>
-        /// <param name="original">The original string</param>
-        /// <returns>String</returns>
-        /// <author>Vu Tran</author>
-        /// <date>05/26/2015</date>
         public static string EncodeSpace(string original)
         {
             return original.Replace(' ', '\u00a0');
