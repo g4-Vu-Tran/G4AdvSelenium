cc3890b5546099ff6cfd286ac8aa877e21bbf237
g4-Vu-Tran <vu.tran@logigear.com>
2016-05-31 23:49:27 +0700

    cb

---
+++ SeleniumAdvProject/SeleniumAdvProject/PageObjects/DataProfileBasePage.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using SeleniumAdvProject.Common;
+using SeleniumAdvProject.DataObjects;
+using OpenQA.Selenium.Support.UI;
+using SeleniumAdvProject.Ultilities.Controls;
+using System.Threading;
+
+namespace SeleniumAdvProject.PageObjects
+{
+    public class DataProfileBasePage : BasePage
+    {
+        #region Locators
+        protected static readonly By _btnNext = By.XPath("//input[@value='Next' and @type='button']");
+        protected static readonly By _btnFinish = By.XPath("//input[@value='Finish' and @type='button']");
+        protected static readonly By _btnCancel = By.XPath("//input[@value='Cancel' and @type='button']");
+        protected static readonly By _btnBack = By.XPath("//input[@value='Back' and @type='button']");
+        #endregion
+
+        #region Elements
+        public Button BtnNext
+        {
+            get { return new Button(_webDriver.FindElement(_btnNext)); }
+        }
+        public Button BtnFinish
+        {
+            get { return new Button(_webDriver.FindElement(_btnFinish)); }
+        }
+        public Button BtnCancel
+        {
+            get { return new Button(_webDriver.FindElement(_btnCancel)); }
+        }
+        public Button BtnBack
+        {
+            get { return new Button(_webDriver.FindElement(_btnBack)); }
+        }
+        #endregion
+
+        #region Methods
+        public DataProfileBasePage() : base() { }
+
+        public DataProfileBasePage(IWebDriver webDriver) : base(webDriver) { }
+
+        /// <summary>
+        /// Click on the button
+        /// </summary>
+        /// <param name="buttonName">Name of the button</param>
+        /// <author>Vu Tran</author>
+        /// <date>05/30/2016</date>
+        protected void ClickOnButton(string buttonName)
+        {
+            switch (buttonName)
+            {
+                case "Back":
+                    BtnBack.Click();
+                    break;
+                case "Next":
+                    BtnNext.Click();
+                    break;
+                case "Finish":
+                    BtnFinish.Click();
+                    break;
+                case "Cancel":
+                    BtnCancel.Click();
+                    break;
+            }
+        }
+
+       // enum AddDataProfileSteps { GeneralSettings, DisplayFields, SortFields, FilterFields,StatisticFields, DisplaySubFields, SortSubFields, FilterSubFields};
+        #endregion
+
+    }
+}
---
+++ SeleniumAdvProject/SeleniumAdvProject/PageObjects/GeneralSettingsPage.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using SeleniumAdvProject.Common;
+using SeleniumAdvProject.DataObjects;
+using OpenQA.Selenium.Support.UI;
+using SeleniumAdvProject.Ultilities.Controls;
+using System.Threading;
+
+namespace SeleniumAdvProject.PageObjects
+{
+    public class GeneralSettingsPage : DataProfileBasePage
+    {
+        #region Locators
+        static readonly By _txtProfileName = By.XPath("//input[@id='txtProfileName']");
+        static readonly By _cbbItemType = By.XPath("//select[@id='cbbEntityType']");
+        static readonly By _cbbRelatedData = By.XPath("//select[@id='cbbSubReport']");
+        #endregion
+
+        #region Elements
+        public TextBox TxtProfileName
+        {
+            get { return new TextBox(_webDriver.FindElement(_txtProfileName)); }
+        }
+        public ComboBox CbbItemType
+        {
+            get { return new ComboBox(_webDriver.FindElement(_cbbItemType)); }
+        }
+        public ComboBox CbbRelatedData
+        {
+            get { return new ComboBox(_webDriver.FindElement(_cbbRelatedData)); }
+        }
+        #endregion
+
+        #region Methods
+        public GeneralSettingsPage() : base() { }
+
+        public GeneralSettingsPage(IWebDriver webDriver) : base(webDriver) { }
+
+        public object SetValidGeneralSettingsValue(string name, string itemType, string relatedData, string button = "Next")
+        {
+
+            TxtProfileName.SendKeys(name);
+            CbbItemType.SelectByText(itemType);
+            CbbRelatedData.SelectByText(relatedData);
+
+            switch (button)
+            {
+                case "Finish": BtnFinish.Click();
+                    return new DataProfilePage(_webDriver);
+                case "Cancel": BtnCancel.Click();
+                    return new DataProfilePage(_webDriver);
+                default: BtnNext.Click();
+                    return new DataProfilePage(_webDriver);
+            }
+        }
+
+        #endregion
+
+    }
+}
