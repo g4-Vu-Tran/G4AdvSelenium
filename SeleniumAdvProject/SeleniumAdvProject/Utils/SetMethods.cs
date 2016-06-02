2a891f07fe046e726fbc664c1c022c6a048cfba4
g4-Vu-Tran <vu.tran@logigear.com>
2016-06-03 00:57:51 +0700

    extentions

---
+++ SeleniumAdvProject/SeleniumAdvProject/Utils/SetMethods.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Interactions;
+
+namespace SeleniumAdvProject.Utils
+{
+    class SetMethods
+    {
+        public static void EnterText(IWebElement element, string value)
+        {
+            if (value != null)
+                element.SendKeys(value);
+        }
+
+        public static void Clicks(IWebElement element)
+        {
+            element.Click();
+        }
+
+        public static void SelectDropDown(IWebElement element, string value)
+        {
+            if (value != null)
+                new SelectElement(element).SelectByText(value);
+        }
+
+        public static void SelectOptionByLabel(IWebElement element, string label, string option)
+        {
+            if (label == null)
+            {
+                SelectDropDown(element, option);
+                return;
+            }
+            IList<IWebElement> optgroups = element.FindElements(By.TagName("optgroup"));
+            foreach (IWebElement optgroup in optgroups)
+            {
+                if (optgroup.GetAttribute("label").Equals(label))
+                {
+                    IList<IWebElement> options = optgroup.FindElements(By.TagName("option"));
+                    foreach (IWebElement opt in options)
+                    {
+                        if (opt.GetAttribute("value").Equals(option))
+                        {
+                            opt.Click();
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void Check(IWebElement element)
+        {
+            if (!element.Selected)
+                element.Click();
+        }
+
+        public static void UnCheck(IWebElement element)
+        {
+            if (element.Selected)
+                element.Click();
+        }
+
+        public static void MouseTo(IWebElement element)
+        {
+            IWebDriver webDriver = ((RemoteWebElement)element).WrappedDriver;
+            Actions actions = new Actions(webDriver);
+            actions.MoveToElement(element).Build().Perform();
+        }
+
+
+    }
+}
---
+++ SeleniumAdvProject/SeleniumAdvProject/Utils/TableData.cs
@@ -0,0 +1,103 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAdvProject.Utils
+{
+    public class TableData
+    {
+        private static List<TableDataColection> _tableDataColections = new List<TableDataColection>();
+
+        private static void ReadTable(IWebElement table)
+        {
+            //Get all the columns from the table
+            var columns = table.FindElements(By.TagName("th"));
+
+            //Get all the rows from the table
+            var rows = table.FindElements(By.TagName("tr"));
+
+            //Create row index
+            int rowIndex = 0;
+
+            foreach (var row in rows)
+            {
+                int colIndex = 0;
+                var colDatas = row.FindElements(By.TagName("td"));
+
+                foreach (var colValue in colDatas)
+                {
+                    _tableDataColections.Add(new TableDataColection
+                    {
+                        RowNumber = rowIndex,
+                        ColumnName = columns[colIndex].Text != "" ?
+                                      columns[colIndex].Text : colIndex.ToString(),
+                        ColumnValue = colValue.Text,
+                        ColumnSpecialValues = colValue.Text != "" ? colValue.FindElements(By.TagName("input")) : colValue.FindElements(By.TagName("a"))
+                    });
+
+                    //Move to next column
+                    colIndex++;
+                }
+                rowIndex++;
+            }
+        }
+
+        public static string ReadCell(IWebElement table, string columnName, int rowNumber)
+        {
+            ReadTable(table);
+            var data = (from e in _tableDataColections
+                        where e.ColumnName == columnName && e.RowNumber == rowNumber
+                        select e.ColumnValue).SingleOrDefault();
+            return data;
+        }
+
+        public static void PerformActionOnCell(IWebElement table, string columnIndex, string refColumnName, string refColumnValue, string controlToOperate = null)
+        {
+            ReadTable(table);
+            foreach (int rowNumber in GetRowNumber(refColumnName, refColumnValue))
+            {
+                var cell = (from e in _tableDataColections
+                            where e.ColumnName == columnIndex && e.RowNumber == rowNumber
+                            select e.ColumnSpecialValues).SingleOrDefault();
+
+
+                if (controlToOperate != null && cell != null)
+                {
+                    var returnedControl = (from c in cell
+                                           where c.GetAttribute("value") == controlToOperate
+                                           select c).SingleOrDefault();
+                    returnedControl.Click();
+                }
+                else
+                {
+                    cell.First().Click();
+                }
+            }
+        }
+
+        private static IEnumerable GetRowNumber(string columnName, string columnValue)
+        {
+            foreach (var table in _tableDataColections)
+            {
+                if (table.ColumnName == columnName && table.ColumnValue == columnValue)
+                    yield return table.RowNumber;
+            }
+        }
+    }
+
+    public class TableDataColection
+    {
+        public int RowNumber { get; set; }
+
+        public string ColumnName { get; set; }
+
+        public string ColumnValue { get; set; }
+
+        public IEnumerable<IWebElement> ColumnSpecialValues { get; set; }
+
+    }
+}
