bd4d0c5a373b96c40c51d43b6d81847b683e9c9d
g4-Tu-Nguyen <lieutunguyen@gmail.com>
2016-06-09 23:00:28 +0700

    commit

--- SeleniumAdvProject/SeleniumAdvProject/Common/Constants.cs
+++ SeleniumAdvProject/SeleniumAdvProject/Common/Constants.cs
@@ -20,8 +20,7 @@ namespace SeleniumAdvProject.Common
         public const string LowerCaseUser = "admin";
 
         //URL
-        //public const string LoginPageUrl = "http://192.168.1.104/TADashboard/login.jsp";
-        public const string LoginPageUrl = "http://localhost:54000/TADashboard/login.jsp";
+        public const string LoginPageUrl = "http://192.168.1.104/TADashboard/login.jsp";
         //public const string LoginPageUrl = "http://groupba.dyndns.org:54000/TADashboard/login.jsp";
         public const string MainPageUrl = "/TADashboard/";
 
